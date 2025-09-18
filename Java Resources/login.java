6ae463b22b53ff925a87006ceb686813629f0017
Priyanshu Kumar Pandey <contact.priyanshupandey@gmail.com>
2025-09-18 17:04:12 +0000

    Authentication Codebase

---
+++ Java Resources/login.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.RequestDispatcher;
+
+@WebServlet("/login")
+public class login extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+            throws ServletException, IOException {
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if ("priyanshu".equals(username) && "123".equals(password)) {
+            // Store username in request scope
+            req.setAttribute("user", username);
+
+            // Forward to profile servlet
+            RequestDispatcher rd = req.getRequestDispatcher("profile");
+            rd.forward(req, resp);
+        } else {
+            resp.setContentType("text/html");
+            resp.getWriter().println("<h3 style='color:red;'>Invalid username or password</h3>");
+            RequestDispatcher rd = req.getRequestDispatcher("index.html");
+            rd.include(req, resp);
+        }
+    }
+}
---
+++ Java Resources/profile.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/profile")
+public class profile extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+            throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        String username = (String) req.getAttribute("user");
+
+   
+        out.println("Welcome to your profile, " + username);
+   
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        doPost(req, resp);
+    }
+}
